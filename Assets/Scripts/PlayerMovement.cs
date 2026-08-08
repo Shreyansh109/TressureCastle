9a774f84eeac69666d720503cfeb5857b9ed23d8
Shreyansh <shreyanshgupta118@gmail.com>
2026-08-08 13:27:05 +0530

    player move using linearVelocity at 2x

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,19 +3,27 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     Vector2 movementInput;
+    Vector2 playerMovement;
+    Rigidbody2D rb;
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        Run();
     }
 
     void OnMove(InputValue value)
     {
         movementInput = value.Get<Vector2>();
-        print(movementInput);
+    }
+
+    void Run()
+    {
+        playerMovement = new Vector2(movementInput.x, 0f);
+        rb.linearVelocity = playerMovement * 2.5f;
     }
 }
