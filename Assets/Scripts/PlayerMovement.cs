f2ddc85f0ec8975ca73d1ee3af5e68add37fdb3e
Shreyansh <shreyanshgupta118@gmail.com>
2026-08-18 23:47:48 +0530

    player bounce on mushroom --value=1.5, multiply

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -112,7 +112,7 @@ public class PlayerMovement : MonoBehaviour
     void OnCollisionEnter2D(Collision2D collision)
     {
         isGrounded = true;
-        isJump = false;
+        //isJump = false;
     }
     void OnCollisionExit2D(Collision2D collision)
     {
