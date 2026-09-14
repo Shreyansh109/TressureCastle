e2f0db479e5f6a0e7e7dc68126d566702b925dcd
Shreyansh <shreyanshgupta118@gmail.com>
2026-09-14 23:07:23 +0530

    assign death canvas in level 2

--- Assets/Scripts/LevelDesignerScript.cs
+++ Assets/Scripts/LevelDesignerScript.cs
@@ -1,14 +1,14 @@
-// using UnityEngine;
-// using UnityEngine.SceneManagement;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 
-// public class LevelDesignerScript : MonoBehaviour
-// {
-//     [SerializeField] private ;
-//     void Start()
-//     {
-//         if(UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex == 0)
-//         {
-            
-//         }
-//     }
-// }
+public class LevelDesignerScript : MonoBehaviour
+{
+    [SerializeField] private GameObject coinCanvas;
+    void Start()
+    {
+        if(UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex == 0)
+        {
+            coinCanvas.gameObject.SetActive(false);
+        }
+    }
+}
